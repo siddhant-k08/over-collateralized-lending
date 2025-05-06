ec820dd6363e6b5a5d5cab0a7f13bb1ade2c5930
Siddhant Kulkarni <124372481+siddhant-k08@users.noreply.github.com>
2025-05-06 23:01:07 +0530

    Update page.tsx

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -16,8 +16,7 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-            <span className="block text-xl font-bold">(SpeedRunEthereum Over-collateralized Lending extension)</span>
+            <span className="block text-4xl font-bold">Over-collateralized Lending</span>
           </h1>
           <div className="flex justify-center items-center space-x-2 flex-col sm:flex-row">
             <p className="my-2 font-medium">Connected Address:</p>
